7cdd4b7506f12ac2e811d9d5483f3892cf7404ad
NaveenB2004 <naveennbalasooriya2004@gmail.com>
2023-05-31 10:26:08 +0530

    updates

--- Updater/src/main/java/Updater.java
+++ Updater/src/main/java/Updater.java
@@ -6,16 +6,11 @@
 public class Updater {
 
     public static void main(String[] args) {
-        int x = 0;
         try {
             String y = args[0];
-            x++;
+            new process.process().installer();
         } catch (Exception e) {
             System.out.println(e);
-        }
-        if (x != 0) {
-            new process.process().installer();
-        } else {
             new process.process().checkUpdates();
         }
     }
