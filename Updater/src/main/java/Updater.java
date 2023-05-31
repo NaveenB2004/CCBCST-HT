efc9edc141bf68ec6672a40ae7436ac797d2fa4c
NaveenB2004 <naveennbalasooriya2004@gmail.com>
2023-05-31 09:45:24 +0530

    bug fix n updates

--- Updater/src/main/java/Updater.java
+++ Updater/src/main/java/Updater.java
@@ -1,6 +1,4 @@
 
-import com.formdev.flatlaf.FlatDarkLaf;
-
 /**
  *
  * @author NaveenB2004
@@ -9,12 +7,9 @@ public class Updater {
 
     public static void main(String[] args) {
         try {
-            System.out.println(args[0]);
             new process.process().installer();
         } catch (Exception e) {
-            System.out.println(e);
             new process.process().checkUpdates();
-            FlatDarkLaf.setup();
         }
     }
 }
--- Updater/src/main/java/process/prograss.java
+++ Updater/src/main/java/process/prograss.java
@@ -1,5 +1,7 @@
 package process;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLaf;
 import java.awt.Toolkit;
 
 /**
@@ -13,7 +15,13 @@ public class prograss extends javax.swing.JFrame {
      */
     public prograss() {
         initComponents();
+        startup();
         this.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/scout logo.png")));
+    }
+    
+    private void startup(){
+            FlatDarkLaf.setup();
+            FlatLaf.updateUI();
     }
 
     /**
